8ba456bcd008d011122c5067ec8d60feae0d3b9c
wind <20213802007@m.scnu.edu.cn>
2023-12-12 16:51:41 +0800

    Bind button event (Switch on and off).

--- app/src/main/java/com/example/store/DeviceAdapter.kt
+++ app/src/main/java/com/example/store/DeviceAdapter.kt
@@ -3,32 +3,40 @@ package com.example.store
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.example.store.controlcenter.Operation
 import com.example.store.device.Device
 
-class DeviceAdapter(val deviceList:ArrayList<Device>):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class DeviceAdapter(val deviceList: ArrayList<Device>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val deviceName:TextView = view.findViewById(R.id.deviceName)
+        val deviceName: TextView = view.findViewById(R.id.deviceName)
+        val deviceSwitch: Switch = view.findViewById(R.id.deviceSwitch)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.device_item, parent,false)
-
-//        val displayMetrics = parent.context.resources.displayMetrics
-//        val screenWidth = displayMetrics.widthPixels
-//
-//        view.layoutParams = ViewGroup.LayoutParams(screenWidth / 2, ViewGroup.LayoutParams.WRAP_CONTENT)
-
+            .inflate(R.layout.device_item, parent, false)
         return ViewHolder(view)
     }
 
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val device = deviceList[position]
         if (holder is ViewHolder) {
+            val device = deviceList[position]
             holder.deviceName.text = device.name
+            holder.deviceSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+                    if (isChecked) {
+                        device.changeOperation(Operation(device.deviceID, "turnOn", 0))
+                        Toast.makeText(buttonView.context, "${device.deviceID} turnOn", Toast.LENGTH_SHORT).show()
+                    } else {
+                        device.changeOperation(Operation(device.deviceID, "turnOff", 0))
+                        Toast.makeText(buttonView.context, "turnOff", Toast.LENGTH_SHORT).show()
+                    }
+            }
         }
     }
 
