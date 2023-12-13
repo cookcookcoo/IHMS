fb733fdfd4b9af4ef8270fc73711be7fec530d43
wind <20213802007@m.scnu.edu.cn>
2023-12-13 15:02:21 +0800

    添加两个DeviceDetail的Activity和Layout，Light和TV的详情页

---
+++ app/src/main/java/com/example/store/DeviceDetail/LightDetail.kt
@@ -0,0 +1,12 @@
+package com.example.store.DeviceDetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.store.R
+
+class LightDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_light_detail)
+    }
+}
--- app/src/main/java/com/example/store/DeviceDetail/TelevisionDetail.kt
+++ app/src/main/java/com/example/store/DeviceDetail/TelevisionDetail.kt
@@ -1,11 +1,12 @@
-package com.example.store
+package com.example.store.DeviceDetail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.store.R
 
-class DeviceDetail : AppCompatActivity() {
+class TelevisionDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_device_detail)
+        setContentView(R.layout.activity_television_detail)
     }
 }
