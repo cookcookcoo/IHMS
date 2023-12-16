d2da7c529da709abb6c5a607a534e70569211917
cookcookcoo <1468023504@qq.com>
2023-12-16 17:07:53 +0800

    写完了风扇、电视、空调的详情页布局

--- app/src/main/java/com/example/store/deviceDetail/AirConditionerDetail.kt
+++ app/src/main/java/com/example/store/deviceDetail/AirConditionerDetail.kt
@@ -2,12 +2,26 @@ package com.example.store.deviceDetail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.example.store.R
 
 class AirConditionerDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_air_conditioner_detail)
+
+
+        //        启用导航栏的返回键
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 启用返回按钮
+        supportActionBar?.setDisplayShowHomeEnabled(true) // 显示返回按钮
+
+        // 设置导航按钮点击监听
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
 
     }
 }
